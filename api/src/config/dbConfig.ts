a8e921dfe499580781ed0d784f7f19660747e8d5
Stefan Fischer <stefischer@mail.com>
2024-05-27 03:40:33 -0400

    Create dbConfig

---
+++ api/src/config/dbConfig.ts
@@ -0,0 +1,24 @@
+import type { DataSourceOptions } from 'typeorm';
+
+import dotenv from 'dotenv';
+import * as Entities from '@@entities/index.js';
+
+dotenv.config();
+
+const DB_PORT = parseInt(process.env.DB_PORT!, 10);
+
+const dbConfig: DataSourceOptions = {
+    type: "postgres",
+    host: process.env.DB_HOST!,
+    port: DB_PORT,
+    database: process.env.DB_NAME!,
+    username: process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD!,
+    synchronize: true,
+    logging: false,
+    entities: Entities,
+    migrations: [],
+    subscribers: []
+};
+
+export default dbConfig;
