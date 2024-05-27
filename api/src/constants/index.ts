314ba5f30632f22e08a2dbffa8a4e07bc0f6e3ed
Stefan Fischer <stefischer@mail.com>
2024-05-27 10:00:22 -0400

    Add logChannel constants

--- api/src/constants/index.ts
+++ api/src/constants/index.ts
@@ -1,3 +1,4 @@
 export * as DEFAULTS from './defaults.js';
 export * as ENV from './environment.js';
 export * as ERRORS from './errorMessages.js';
+export * as LOG_CHANNELS from './logChannels.js';
