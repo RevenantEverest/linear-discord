b0b6b710456cb82921e8179e816ab807de275cff
Stefan Fischer <stefischer@mail.com>
2024-05-27 03:41:34 -0400

    Add util methods

---
+++ api/src/utils/index.ts
@@ -0,0 +1,8 @@
+export * as colors from './colors.js';
+export * as common from './common.js';
+export * as entities from './entities.js';
+export * as errors from './errors.js';
+export * as logs from './logs.js';
+export * as pagination from './pagination.js';
+export * as promises from './promises.js';
+export * as validation from './validation.js';
---
+++ api/src/utils/logs.ts
@@ -0,0 +1,14 @@
+import type { LogOptions, ErrorLogOptions } from '@@types/logs.js';
+
+import chalk from 'chalk';
+import * as colors from './colors.js';
+
+export async function log({ color, type="LOG", message="" }: LogOptions) {
+    const logType = "[" + type + "]";
+    return console.log(chalk.hex(color.toString())(logType) + " " + message);
+};
+
+export async function error({ color=colors.error, type="ERROR", message="", err }: ErrorLogOptions) {
+    const logType = "[" + type + "]";
+    return console.error(chalk.hex(color.toString())(logType) + " " + message, err);
+};
