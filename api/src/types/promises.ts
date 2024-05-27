b19e9c6346fbb30198f2e5be4752da56d2d0e81c
Stefan Fischer <stefischer@mail.com>
2024-05-27 10:01:50 -0400

    Add waitFor method in promises utils

--- api/src/types/promises.ts
+++ api/src/types/promises.ts
@@ -2,3 +2,8 @@
 export type PromiseSuccess<T> = T | null | undefined;
 export type PromiseError<T> = T | undefined;
 export type PromiseTuple<T, D = Error> = Promise<[PromiseSuccess<T>, PromiseError<D>]>;
+
+export interface WaitForOptions {
+    intervalLength?: number,
+    retries?: number
+};
--- api/src/utils/promises.ts
+++ api/src/utils/promises.ts
@@ -1,4 +1,7 @@
-import { PromiseError, PromiseTuple } from '@@types/promises.js';
+import { PromiseError, PromiseTuple, WaitForOptions } from '@@types/promises.js';
+
+import * as logs from './logs.js';
+import * as colors from './colors.js';
 
 export async function handle<T, D = Error>(promise: Promise<T>): PromiseTuple<T, D> {
     return promise
@@ -8,4 +11,29 @@ export async function handle<T, D = Error>(promise: Promise<T>): PromiseTuple<T,
     .catch((err: PromiseError<D>) => {
         return [undefined, err]
     });
+};
+
+export async function waitFor(conditionFunction: () => boolean, options?: WaitForOptions) {
+    
+    let retries = options?.retries ?? 5;
+    const poll = (resolve: (value?: unknown) => void) => {
+        if(retries < 0) {
+            logs.error({ 
+                color: colors.error, 
+                type: "ERROR", 
+                message: "waitFor method out of retries" 
+            });
+            process.exit(1);
+        }
+
+        if(conditionFunction()) {
+            resolve();
+        }
+        else {
+            retries -= 1;
+            setTimeout(() => poll(resolve), options?.intervalLength ?? 400);
+        }
+    };
+
+    return new Promise(poll);
 };
