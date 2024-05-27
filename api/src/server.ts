b69117665061fbd97c3525cffaeb7b54b2cab71d
Stefan Fischer <stefischer@mail.com>
2024-05-27 10:02:29 -0400

    Add discord bot login to server

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,12 +3,20 @@ import "reflect-metadata";
 import AppDataSource from '@@db/dataSource.js';
 import waitForPostgres from '@@db/waitForPostgres.js';
 import initializeApp from './app.js';
+import bot from './discordBot.js';
 
 import { ENV } from '@@constants/index.js';
-import { logs, colors } from '@@utils/index.js';
+import { logs, colors, promises } from '@@utils/index.js';
 
 (async function main() {
     await waitForPostgres(AppDataSource);
+
+    bot.login(ENV.DISCORD.KEY);
+
+    await promises.waitFor(() => bot.isReady(), {
+        retries: 10,
+        intervalLength: 2000
+    });
 
     const PORT = ENV.API_PORT || 3001;
     const app = initializeApp();
