6a3df6d65007e4450821fd84ad664c989646207e
Stefan Fischer <stefischer@mail.com>
2024-05-28 06:34:08 -0400

    Add colors and new method to colors utils

--- api/src/utils/colors.ts
+++ api/src/utils/colors.ts
@@ -7,6 +7,22 @@ export const error = 0xff0000;
 export const topgg = 0xff3366;
 export const banana = 0xffcc00;
 
+export const linear = 0x5E6AD2;
+export const linearRestore = 0x7BFB7B;
+
+/**
+ * COnverts hex color string to hexadecimal.
+ *
+ * *Example:*
+ *
+ * "#00FF00" -> 0x00FF00 
+ * @param hexColor - string
+ * @returns 
+ */
+export function hexColorToHexValue(hexColor: string): number {
+    return parseInt(hexColor.substring(1), 16);
+};
+
 /**
  * Grabs a list of all variables exported by this file, and returns a random one
  * @param this 
