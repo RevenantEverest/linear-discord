80d5090237ea34a77403633b42c24c5f1251576c
Stefan Fischer <stefischer@mail.com>
2024-05-28 06:35:19 -0400

    Add linear constants

--- api/src/constants/index.ts
+++ api/src/constants/index.ts
@@ -1,4 +1,5 @@
 export * as DEFAULTS from './defaults.js';
 export * as ENV from './environment.js';
 export * as ERRORS from './errorMessages.js';
+export * as LINEAR from './linear.js';
 export * as LOG_CHANNELS from './logChannels.js';
