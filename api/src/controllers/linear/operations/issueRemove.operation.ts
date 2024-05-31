04eb3bd36bb6b042a99c845bd01a92fdcb92c7c4
Stefan Fischer <stefischer@mail.com>
2024-05-31 08:43:30 -0400

    Create issueRemove operation

---
+++ api/src/controllers/linear/operations/issueRemove.operation.ts
@@ -0,0 +1,17 @@
+import type { LinearIssue } from '@@types/linear/linearIssue.js';
+import type { EmbedBuilder } from 'discord.js';
+
+import Discord from 'discord.js';
+import { colors } from '@@utils/index.js';
+
+function issueRemove(embed: EmbedBuilder, issue: LinearIssue): EmbedBuilder {
+
+    embed
+    .setColor(colors.error)
+    .setAuthor({ name: "" })
+    .setTitle(`**${issue.team.key}-${issue.number}** ${issue.title}`)
+
+    return embed;
+};
+
+export default issueRemove;
