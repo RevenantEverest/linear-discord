3d478026997c6f89d490bca1a9d60e237788bc8b
Stefan Fischer <stefischer@mail.com>
2024-05-31 08:44:04 -0400

    Add operations to index

---
+++ api/src/controllers/linear/operations/index.ts
@@ -0,0 +1,4 @@
+export { default as issueCreate } from './issueCreate.operation.js';
+export { default as issueRemove } from './issueRemove.operation.js';
+export { default as issueRestore } from './issueRestore.operation.js';
+export { default as issueUpdate } from './issueUpdate.operation.js';
