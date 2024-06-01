51c1f694f530c8b804ab1e3a73eb7b32944956a6
Stefan Fischer <stefischer@mail.com>
2024-06-01 13:39:38 -0400

    Populate onError discord event handler

--- api/src/controllers/discordEvents/onError.ts
+++ api/src/controllers/discordEvents/onError.ts
@@ -1,7 +1,16 @@
 import type { Client } from 'discord.js';
 
-function onError(bot: Client, err: Error) {
+import { ENV } from '@@constants/index.js';
+import { logs, colors } from '@@utils/index.js';
 
+function onError(bot: Client, err: Error) {
+    const message = "CLIENT ERROR - API";
+
+    if(ENV.IS_DEV) {
+        return logs.error({ color: colors.error, err, message: message });
+    };
+
+    logs.postToLogsChannel({ bot: bot, color: colors.error, title: "CLIENT ERROR - API" });
 };
 
 export default onError;
