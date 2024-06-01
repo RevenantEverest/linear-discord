486272184edfa496b3bf542b368fde5a1d35e31c
Stefan Fischer <stefischer@mail.com>
2024-06-01 09:13:50 -0400

    Add controllers to index

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,2 +1,4 @@
 export * as discordEventsController from './discordEvents/index.js';
 export * as linearController from './linear/index.js';
+export * as linearWebhooksController from './linearWebhooks/index.js';
+export * as authControllers from './auth/index.js';
